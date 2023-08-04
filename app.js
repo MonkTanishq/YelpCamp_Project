32e04c5c335ce1bf91c69ce538c091e8910938b3
TANISHQ CHAUHAN <121804467+MonkTanishq@users.noreply.github.com>
2023-08-04 13:49:54 +0530

    Commit for Getting Error in the Connection
    Error: [nodemon] app crashed - waiting for file changes before starting...
    This was the ERROR, I am getting

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const path = require('path')
 const mongoose = require('mongoose')
-// const Campground = require('./models/campground')
-// const {transcode} = require('buffer');
+const Campground = require('./models/campground')
+const {transcode} = require('buffer');
 
-// mongoose.set('strictQuery', true);
-// mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useUnifiedTopology: true
-// });
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -25,11 +25,11 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-// app.get('/makecampground', async(req, res) => {
-//     const camp = new Campground({title: 'My backyard', description: 'cheap camping'});
-//     await camp.save();
-//     res.send(camp)
-// })
+app.get('/makecampground', async(req, res) => {
+    const camp = new Campground({title: 'My backyard', description: 'cheap camping'});
+    await camp.save();
+    res.send(camp)
+})
 
 app.listen(3000, () => {
     console.log("Serving on port 3000")
