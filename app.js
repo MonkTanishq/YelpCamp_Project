49e61f5a523a1aee90dec809b7ee2b53afebaef3
TANISHQ CHAUHAN <121804467+MonkTanishq@users.noreply.github.com>
2023-09-13 11:25:33 +0530

    Commit for JOI Validation Middleware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const Joi = require('joi');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError =  require('./utils/ExpressError');
 const methodOverride = require('method-override');
@@ -32,6 +33,16 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
 app.get('/', (req, res) => {
     res.render('home')
 });
@@ -46,7 +57,8 @@ app.get('/campgrounds/new', (req,res) => {
 })
 
 app.post('/campgrounds', catchAsync(async(req, res, next) => {
-    if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+    //if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+
    const campground = new Campground(req.body.campground);
    await campground.save();
    res.redirect(`/campgrounds/${campground._id}`)
