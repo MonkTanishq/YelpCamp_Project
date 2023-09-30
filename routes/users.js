986966f12e34e849ebe454a7c106f94cb8f66121
TANISHQ CHAUHAN <121804467+MonkTanishq@users.noreply.github.com>
2023-09-30 20:33:06 +0530

    Commit for "returnTo Middleware Update"

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
+const { storeReturnTo } = require('../middleware');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
@@ -13,6 +14,7 @@ router.post('/register', catchAsync(async (req, res, next) => {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
+        //Below for Fixing Register Route
         req.login(registeredUser, err => {
             if (err) return next(err);
             req.flash('success', 'Welcome to Yelp Camp!');
@@ -28,9 +30,12 @@ router.get('/login', (req, res) => {
     res.render('users/login');
 })
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+ // use the storeReturnTo middleware to save the returnTo value from session to res.locals
+ // passport.authenticate logs the user in and clears req.session
+ // Now we can use res.locals.returnTo to redirect the user after login
+router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     req.flash('success', 'welcome back!');
-    const redirectUrl = req.session.returnTo || '/campgrounds';
+    const redirectUrl = req.locals.returnTo || '/campgrounds';
     delete req.session.returnTo;
     res.redirect(redirectUrl);
 })
