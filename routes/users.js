46c7ae4ca36b004d89a192c9cc6ec566a05c4336
TANISHQ CHAUHAN <121804467+MonkTanishq@users.noreply.github.com>
2023-10-03 14:34:12 +0530

    Commit for "Refactoring the User"
    Adding the User Controller

--- routes/users.js
+++ routes/users.js
@@ -4,58 +4,19 @@ const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 const { storeReturnTo } = require('../middleware');
+const users = require('../controllers/users');
 
-router.get('/register', (req, res) => {
-    res.render('users/register');
-});
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
-router.post('/register', catchAsync(async (req, res, next) => {
-    try {
-        const { email, username, password } = req.body;
-        const user = new User({ email, username });
-        const registeredUser = await User.register(user, password);
-        //Below for Fixing Register Route
-        req.login(registeredUser, err => {
-            if (err) return next(err);
-            req.flash('success', 'Welcome to Yelp Camp!');
-            res.redirect('/campgrounds');
-        })
-    } catch (e) {
-        req.flash('error', e.message);
-        res.redirect('register');
-    }
-}));
-
-router.get('/login', (req, res) => {
-    res.render('users/login');
-})
-
- // use the storeReturnTo middleware to save the returnTo value from session to res.locals
+// use the storeReturnTo middleware to save the returnTo value from session to res.locals
  // passport.authenticate logs the user in and clears req.session
  // Now we can use res.locals.returnTo to redirect the user after login
-router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-    req.flash('success', 'welcome back!');
-    const redirectUrl = req.session.returnTo || '/campgrounds';
-    delete req.session.returnTo;
-    res.redirect(redirectUrl);
-})
+router.route('/login')
+    .get(users.renderLogin)
+    .post(storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
 
-// router.get('/logout', (req, res) => {
-//     req.logout();
-//     req.flash('success', "Goodbye!");
-//     res.redirect('/campgrounds');
-// })
-
-//Above Commented Piece of code will give error sometimes because of passport.js is updated. So the below code will be work correctly.
-
-router.get('/logout', (req, res, next) => {
-    req.logout(function (err) {
-        if (err) {
-            return next(err);
-        }
-        req.flash('success', 'Goodbye!');
-        res.redirect('/campgrounds');
-    });
-}); 
+router.get('/logout', users.logout)
 
 module.exports = router;
